89c7889bdd3f822bc553336ef9bbd43fb1bcad13
Frank Santolin <frank@cadha.com>
2022-01-02 12:26:01 +0000

    part 1 milestone 4 replace memory cache with external service - api-clusters

--- api-clusters/src/main/java/com/twa/flights/api/clusters/serializer/CitySerializer.java
+++ api-clusters/src/main/java/com/twa/flights/api/clusters/serializer/CitySerializer.java
@@ -2,34 +2,33 @@ package com.twa.flights.api.clusters.serializer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twa.flights.api.clusters.dto.CityDTO;
 import org.springframework.data.redis.serializer.RedisSerializer;
-
-import com.twa.flights.api.clusters.dto.ClusterSearchDTO;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import java.io.IOException;
 
-public class ClusterSearchSerializer implements RedisSerializer<ClusterSearchDTO> {
+public class CitySerializer implements RedisSerializer<CityDTO> {
 
     private final ObjectMapper objectMapper;
 
-    public ClusterSearchSerializer(ObjectMapper objectMapper) {
+    public CitySerializer(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
     @Override
-    public byte[] serialize(ClusterSearchDTO clusterSearch) {
+    public byte[] serialize(CityDTO cityDTO) throws SerializationException {
         try {
-            return objectMapper.writeValueAsBytes(clusterSearch);
+            return objectMapper.writeValueAsBytes(cityDTO);
         } catch (JsonProcessingException e) {
             throw new SerializationException(e.getMessage());
         }
     }
 
     @Override
-    public ClusterSearchDTO deserialize(byte[] bytes) {
+    public CityDTO deserialize(byte[] bytes) throws SerializationException {
         try {
-            return objectMapper.readValue(bytes, ClusterSearchDTO.class);
+            return objectMapper.readValue(bytes, CityDTO.class);
         } catch (IOException e) {
             throw new SerializationException(e.getMessage());
         }
