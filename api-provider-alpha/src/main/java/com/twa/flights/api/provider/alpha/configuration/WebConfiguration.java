1e66b7cae5b5f584d03121033ae3dfdd73913878
Frank Santolin <frank@cadha.com>
2022-01-08 12:19:22 +0000

    part 2 milestone 4 remove NULL values from JSON payloads

--- api-provider-alpha/src/main/java/com/twa/flights/api/provider/alpha/configuration/WebConfiguration.java
+++ api-provider-alpha/src/main/java/com/twa/flights/api/provider/alpha/configuration/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.twa.flights.api.provider.alpha.configuration;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -23,6 +24,7 @@ public class WebConfiguration implements WebMvcConfigurer {
             if (converter instanceof MappingJackson2HttpMessageConverter) {
                 ObjectMapper objectIdMapper = ((MappingJackson2HttpMessageConverter) converter).getObjectMapper();
                 objectIdMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+                objectIdMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
                 objectIdMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             }
         }
@@ -34,6 +36,7 @@ public class WebConfiguration implements WebMvcConfigurer {
         List<HttpMessageConverter<?>> messageConverters = exceptionHandlerExceptionResolver.getMessageConverters();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
         messageConverters.add(new MappingJackson2HttpMessageConverter(objectMapper));
     }
 
